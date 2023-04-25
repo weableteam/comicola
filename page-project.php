72883b184bb4dc080a736f74b811d2284469bce5
Thanh Tien <vothanhtien190901@gmail.com>
2023-04-25 16:09:47 +0700

    push page project

--- page-project.php
+++ page-project.php
@@ -9,7 +9,171 @@
 get_header();
 ?>
 <div class="wyswyg">
-    abc
+    <div class="projects">
+        <div class="container">
+            <div class="pj-top">
+                <div class="name">
+                    <h3>DỰ ÁN ĐANG DIỄN RA</h3>
+                    <p>Các dự án tiêu biểu đang trong quá trình crowdfunding trên Comicola</p>
+                </div>
+
+                <div class="bg-img">
+                    <div class="icon-heart">
+                        <i class="bi bi-heart-fill"></i>
+                    </div>
+                    <div class="title-des">
+                        <h5>DỰ ÁN HOẠT HÌNH CON THỎ</h5>
+                        <p>Coolmate góp 250 triệu đồng giúp Thỏ Bảy Màu lội ngược dòng ngoạn mục: Trở thành phim hoạt
+                            hình Việt Nam đầu tiên gọi vốn cộng đồng thành công</p>
+                        <div class="progress-container progress-primary"><span class="progress-badge"></span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-primary aos-init aos-animate"
+                                    data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000"
+                                    role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                                    style="width: 50%;"></div>
+                            </div>
+                        </div>
+                        <p>Đã ủng hộ 1.550.000.000đ / 3.000.000.000đ</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pj-content">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="" class="item">
+                            <div class="image-box">
+                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/Rectangle 9.webp' ?>"
+                                    alt="">
+                                <span><i class="bi bi-heart"></i></span>
+                            </div>
+
+                            <div class="box-right">
+                                <div class="text-box">
+                                    <h5>DỰ ÁN HOẠT HÌNH CON THỎ</h5>
+                                    <p>Coolmate góp 250 triệu đồng giúp Thỏ Bảy Màu lội ngược dòng ngoạn mục: Trở thành
+                                        phim hoạt hình Việt Nam đầu tiên gọi vốn cộng đồng thành công</p>
+                                </div>
+                                <div class="box-bottom">
+                                    <p>Đã kết thúc</p>
+                                    <div class="progress-container progress-primary"><span
+                                            class="progress-badge"></span>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-primary aos-init aos-animate"
+                                                data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000"
+                                                role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                                                aria-valuemax="100" style="width: 50%;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-6">
+                        <a href="" class="item">
+                            <div class="image-box">
+                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/Rectangle 11.webp' ?>"
+                                    alt="">
+                                <span><i class="bi bi-heart"></i></span>
+                            </div>
+
+                            <div class="box-right">
+                                <div class="text-box">
+                                    <h5>DỰ ÁN HOẠT HÌNH CON THỎ</h5>
+                                    <p>Coolmate góp 250 triệu đồng giúp Thỏ Bảy Màu lội ngược dòng ngoạn mục: Trở thành
+                                        phim hoạt hình Việt Nam đầu tiên gọi vốn cộng đồng thành công</p>
+                                </div>
+                                <div class="box-bottom">
+                                    <p>Đã ủng hộ 1.550.000.000đ / 3.000.000.000đ</p>
+                                    <div class="progress-container progress-primary"><span
+                                            class="progress-badge"></span>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-primary aos-init aos-animate"
+                                                data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000"
+                                                role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                                                aria-valuemax="100" style="width: 50%;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-6">
+                        <a href="" class="item">
+                            <div class="image-box">
+                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/Rectangle 14.webp' ?>"
+                                    alt="">
+                                <span><i class="bi bi-heart"></i></span>
+                            </div>
+
+                            <div class="box-right">
+                                <div class="text-box">
+                                    <h5>DỰ ÁN HOẠT HÌNH CON THỎ</h5>
+                                    <p>Coolmate góp 250 triệu đồng giúp Thỏ Bảy Màu lội ngược dòng ngoạn mục: Trở thành
+                                        phim hoạt hình Việt Nam đầu tiên gọi vốn cộng đồng thành công</p>
+                                </div>
+                                <div class="box-bottom">
+                                    <p>Đã ủng hộ 1.550.000.000đ / 3.000.000.000đ</p>
+                                    <div class="progress-container progress-primary"><span
+                                            class="progress-badge"></span>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-primary aos-init aos-animate"
+                                                data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000"
+                                                role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                                                aria-valuemax="100" style="width: 50%;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-6">
+                        <a href="" class="item">
+                            <div class="image-box">
+                                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/Rectangle 15.webp' ?>"
+                                    alt="">
+                                <span><i class="bi bi-heart"></i></span>
+                            </div>
+
+                            <div class="box-right">
+                                <div class="text-box">
+                                    <h5>DỰ ÁN HOẠT HÌNH CON THỎ</h5>
+                                    <p>Coolmate góp 250 triệu đồng giúp Thỏ Bảy Màu lội ngược dòng ngoạn mục: Trở thành
+                                        phim hoạt hình Việt Nam đầu tiên gọi vốn cộng đồng thành công</p>
+                                </div>
+                                <div class="box-bottom">
+                                    <p>Đã ủng hộ 1.550.000.000đ / 3.000.000.000đ</p>
+                                    <div class="progress-container progress-primary"><span
+                                            class="progress-badge"></span>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-primary aos-init aos-animate"
+                                                data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000"
+                                                role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                                                aria-valuemax="100" style="width: 50%;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="view-all">
+                    <a href="#">1 </a>
+                    <a href="#">2 </a>
+                    <a href="#">3 </a>
+                    <a href="#"><i class="bi bi-caret-right-fill"></i> </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
